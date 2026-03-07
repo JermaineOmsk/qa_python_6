0efc4b946c5802fcd8c1ebefe66a7a8475ddeba3
JermaineOmsk <jermaine1989@yandex.ru>
2026-03-07 15:32:42 +0600

    урл вынесен в отдельный айл

--- conftest.py
+++ conftest.py
@@ -1,10 +1,11 @@
 import pytest
 from selenium import webdriver
+from urls import scooter_url
 @pytest.fixture
 def driver_firefox():
     driver = webdriver.Firefox()
     driver.maximize_window()
-    driver.get('https://qa-scooter.praktikum-services.ru/')
+    driver.get(scooter_url)
     yield driver 
     driver.quit()
 
